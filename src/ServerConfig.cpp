7578b56303a642e5c851378ea037b78d7105425e
Omar Dana <odana@c2r7s2.42beirut.com>
2026-01-02 10:31:01 +0200

    fix

--- src/ServerConfig.cpp
+++ src/ServerConfig.cpp
@@ -3,161 +3,91 @@
 /*                                                        :::      ::::::::   */
 /*   ServerConfig.cpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yitani <yitani@student.42.fr>              +#+  +:+       +#+        */
+/*   By: odana <odana@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/01 10:30:58 by yitani            #+#    #+#             */
-/*   Updated: 2026/01/02 00:45:59 by yitani           ###   ########.fr       */
+/*   Created: 2025/12/22 09:10:18 by odana             #+#    #+#             */
+/*   Updated: 2026/01/02 10:30:49 by odana            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/ServerConfig.hpp"
+#include "ServerConfig.hpp"
 
-ServerConfig::ServerConfig() : ip("0.0.0.0"), serverName(""),
-							   root("./www"), clientMaxBodySize(1048576) {}
-
-ServerConfig::ServerConfig(const ServerConfig &other)
-	: ports(other.ports), ip(other.ip), serverName(other.serverName),
-	  root(other.root), clientMaxBodySize(other.clientMaxBodySize),
-	  errorPages(other.errorPages), locations(other.locations) {}
-
-ServerConfig &ServerConfig::operator=(const ServerConfig &other)
-{
-	if (this != &other)
-	{
-		ports = other.ports;
-		ip = other.ip;
-		serverName = other.serverName;
-		root = other.root;
-		clientMaxBodySize = other.clientMaxBodySize;
-		errorPages = other.errorPages;
-		locations = other.locations;
-	}
-	return *this;
+std::vector<int> ServerConfig::getPorts() const {
+    return ports;
 }
 
-ServerConfig::~ServerConfig() {}
-
-std::vector<int> ServerConfig::getPorts() const
-{
-	return ports;
+std::string ServerConfig::getIP() const {
+    return ip;
 }
 
-std::string ServerConfig::getIP() const
-{
-	return ip;
+std::string ServerConfig::getServerName() const {
+    return serverName;
 }
 
-std::string ServerConfig::getServerName() const
-{
-	return serverName;
+std::string ServerConfig::getRoot() const {
+    return root;
 }
 
-std::string ServerConfig::getRoot() const
-{
-	return root;
+int ServerConfig::getClientMaxBodySize() const {
+    return clientMaxBodySize;
 }
 
-int ServerConfig::getClientMaxBodySize() const
-{
-	return clientMaxBodySize;
+bool ServerConfig::hasErrorPage(int errorCode) const {
+    return errorPages.find(errorCode) != errorPages.end();
 }
 
-bool ServerConfig::hasErrorPage(int errorCode) const
-{
-	return errorPages.find(errorCode) != errorPages.end();
+std::string ServerConfig::getErrorPage(int errorCode) const {
+    if (hasErrorPage(errorCode)) {
+        return errorPages.at(errorCode);
+    }
+    return ""; // could throw MissingErrorPageException
 }
 
-std::string ServerConfig::getErrorPage(int errorCode) const
-{
-	std::map<int, std::string>::const_iterator it = errorPages.find(errorCode);
-	if (it != errorPages.end())
-		return it->second;
-	return "";
+std::vector<LocationConfig> ServerConfig::getLocations() const {
+    return locations;
 }
 
-const LocationConfig *ServerConfig::matchLocation(const std::string &uri) const
-{
-	const LocationConfig *bestMatch = 0;
-	size_t longestMatch = 0;
+const LocationConfig* ServerConfig::matchLocation(const std::string& uri) const {
+    const LocationConfig* match = NULL;
+    size_t longestMatch = 0;
 
-	for (size_t i = 0; i < locations.size(); i++)
-	{
-		std::string locPath = locations[i].getPath();
+    for (size_t i = 0; i < locations.size(); i++) {
+        std::string currentPath = locations[i].getPath();
 
-		if (uri.find(locPath) == 0)
-		{
-			if (locPath.length() > longestMatch)
-			{
-				longestMatch = locPath.length();
-				bestMatch = &locations[i];
-			}
-		}
-	}
-	return bestMatch;
+        if (uri.find(currentPath) == 0) {
+            size_t currentLength = currentPath.length();
+            if (currentLength > longestMatch) {
+                longestMatch = currentLength;
+                match = &locations[i];
+            }
+        }
+    }
+    return match;
 }
 
-std::vector<LocationConfig> ServerConfig::getLocations() const
-{
-	return locations;
-}
+bool ServerConfig::isValid() const {
+    for (size_t i = 0; i < ports.size(); i++) {
+        if (ports.at(i) <= 0 || ports.at(i) > 65535)
+            return false;
+    }
 
-bool ServerConfig::isValid() const
-{
-	if (ports.empty())
-		return false;
+    if (root.empty())
+        return false;
 
-	for (size_t i = 0; i < ports.size(); i++)
-	{
-		if (ports.at(i) <= 0 || ports.at(i) > 65535)
-			return false;
-	}
+    if (clientMaxBodySize <= 0)
+        return false;
 
-	if (root.empty())
-		return false;
+    for (size_t i = 0; i < locations.size(); i++) {
+        if (!locations[i].isValid())
+            return false;
+    }
 
-	if (clientMaxBodySize < 0)
-		return false;
+    for (size_t i = 0; i < locations.size(); i++) {
+        for (size_t j = i + 1; j < locations.size(); j++) {
+            if (locations[i].getPath() == locations[j].getPath())
+                return false;
+        }
+    }
 
-	for (size_t i = 0; i < locations.size(); i++)
-	{
-		if (!locations[i].isValid())
-			return false;
-	}
-
-	return true;
-}
-
-void ServerConfig::addPort(int p)
-{
-	ports.push_back(p);
-}
-
-void ServerConfig::setIP(const std::string &i)
-{
-	ip = i;
-}
-
-void ServerConfig::setServerName(const std::string &name)
-{
-	serverName = name;
-}
-
-void ServerConfig::setRoot(const std::string &r)
-{
-	root = r;
-}
-
-void ServerConfig::setClientMaxBodySize(int size)
-{
-	clientMaxBodySize = size;
-}
-
-void ServerConfig::addErrorPage(int code, const std::string &path)
-{
-	errorPages[code] = path;
-}
-
-void ServerConfig::addLocation(const LocationConfig &loc)
-{
-	locations.push_back(loc);
+    return true;
 }
